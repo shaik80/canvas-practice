7c267a2c6941de383bbdb8c78e2835770e79b2b2
Shaik Mudassir <sh.mudassir98@gmail.com>
2019-09-18 05:52:52 -0700

    height width

--- script/app.js
+++ script/app.js
@@ -4,7 +4,7 @@ const scoreElement = document.getElementById("score");
 
 const ROW = 20;
 const COL = COLUMN = 10;
-const SQ = squareSize = 20;
+const SQ = squareSize = 30;
 let grd = ctx.createLinearGradient(500,0,700,700);
           grd.addColorStop(0,"#8bcbf1");
           grd.addColorStop(1,"white");
